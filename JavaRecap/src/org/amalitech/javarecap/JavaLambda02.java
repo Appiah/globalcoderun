790366bf4b854aee9ffc874090341c56a53bebc0
emap <emap@emmanuels-mbp.mshome.net>
2020-07-09 19:43:39 +0000

    Interface names and methods name updated to reflect the flow a bit more.

--- JavaRecap/src/org/amalitech/javarecap/JavaLambda02.java
+++ JavaRecap/src/org/amalitech/javarecap/JavaLambda02.java
@@ -6,16 +6,16 @@ public class JavaLambda02 {
       JavaLambda02 javaLambda02 = new JavaLambda02();
 		
       //with type declaration
-      MathOperation add = (int a, int b) -> a + b;
+      InterfaceMathOperation add = (int a, int b) -> a + b;
 		
       //with out type declaration
-      MathOperation subtract = (a, b) -> a - b;
+      InterfaceMathOperation subtract = (a, b) -> a - b;
 		
       //with return statement along with curly braces
-      MathOperation multiply = (int a, int b) -> { return a * b; };
+      InterfaceMathOperation multiply = (int a, int b) -> { return a * b; };
 		
       //without return statement and without curly braces
-      MathOperation divide = (int a, int b) -> a / b;
+      InterfaceMathOperation divide = (int a, int b) -> a / b;
 		
       System.out.println("10 + 5 = " + javaLambda02.operate(10, 5, add));
       System.out.println("10 - 5 = " + javaLambda02.operate(10, 5, subtract));
@@ -23,26 +23,24 @@ public class JavaLambda02 {
       System.out.println("10 / 5 = " + javaLambda02.operate(10, 5, divide));
 		
       //without parenthesis
-      GreetingService greetService1 = message ->
-      System.out.println("Hello " + message);
+      MessagingService msgSrvc0 = message -> System.out.println("Hello " + message);
 		
       //with parenthesis
-      GreetingService greetService2 = (message) ->
-      System.out.println("Hello " + message);
+      MessagingService msgSrvc1 = (message) -> System.out.println("Hello " + message);
 		
-      greetService1.sayMessage("Jane");
-      greetService2.sayMessage("Smith");
+      msgSrvc0.dsplyMsg("Jane");
+      msgSrvc1.dsplyMsg("Smith");
    }
 	
-   interface MathOperation {
+   interface InterfaceMathOperation {
       int operation(int a, int b);
    }
 	
-   interface GreetingService {
-      void sayMessage(String message);
+   interface MessagingService {
+      void dsplyMsg(String message);
    }
 	
-   private int operate(int a, int b, MathOperation mathOperation) {
+   private int operate(int a, int b, InterfaceMathOperation mathOperation) {
       return mathOperation.operation(a, b);
    }
 }
