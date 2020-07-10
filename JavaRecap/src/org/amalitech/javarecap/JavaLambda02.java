10fd9daa870cf562ab2f1234b6b46427ac442893
emap <emap@emmanuels-mbp.mshome.net>
2020-07-10 07:53:30 +0000

    Upgraded the parameters to three variable (now includes 'c') with C not included in the actual operation(s).

--- JavaRecap/src/org/amalitech/javarecap/JavaLambda02.java
+++ JavaRecap/src/org/amalitech/javarecap/JavaLambda02.java
@@ -6,21 +6,21 @@ public class JavaLambda02 {
       JavaLambda02 javaLambda02 = new JavaLambda02();
 		
       //with type declaration
-      InterfaceMathOperation add = (int a, int b) -> a + b;
+      InterfaceMathOperation add = (int a, int b, int c) -> a + b;
 		
       //with out type declaration
-      InterfaceMathOperation subtract = (a, b) -> a - b;
+      InterfaceMathOperation subtract = (a, b, c) -> a - b;
 		
       //with return statement along with curly braces
-      InterfaceMathOperation multiply = (int a, int b) -> { return a * b; };
+      InterfaceMathOperation multiply = (int a, int b, int c) -> { return a * b; };
 		
       //without return statement and without curly braces
-      InterfaceMathOperation divide = (int a, int b) -> a / b;
+      InterfaceMathOperation divide = (int a, int b, int c) -> a / b;
 		
-      System.out.println("10 + 5 = " + javaLambda02.operate(10, 5, add));
-      System.out.println("10 - 5 = " + javaLambda02.operate(10, 5, subtract));
-      System.out.println("10 x 5 = " + javaLambda02.operate(10, 5, multiply));
-      System.out.println("10 / 5 = " + javaLambda02.operate(10, 5, divide));
+      System.out.println("10 + 5 = " + javaLambda02.operate(10, 5, 2, add));
+      System.out.println("10 - 5 = " + javaLambda02.operate(10, 5, 2, subtract));
+      System.out.println("10 x 5 = " + javaLambda02.operate(10, 5, 2, multiply));
+      System.out.println("10 / 5 = " + javaLambda02.operate(10, 5, 2, divide));
 		
       //without parenthesis
       MessagingService msgSrvc0 = message -> System.out.println("Hello " + message);
@@ -33,14 +33,14 @@ public class JavaLambda02 {
    }
 	
    interface InterfaceMathOperation {
-      int operation(int a, int b);
+      int operation(int a, int b, int c);
    }
 	
    interface MessagingService {
       void dsplyMsg(String message);
    }
 	
-   private int operate(int a, int b, InterfaceMathOperation mathOperation) {
-      return mathOperation.operation(a, b);
+   private int operate(int a, int b, int c, InterfaceMathOperation mathOperation) {
+      return mathOperation.operation(a, b, c);
    }
 }
